6197b74061a4d2e6c248d2fe07750352f4da07b0
nnacho <githubunity@outlook.com>
2021-12-19 16:33:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿interface IBehaviours
+{
+    IBehaviours Run();
+    IBehaviours RunRight();
+    IBehaviours RunLeft();    
+    IBehaviours Stop();
+    IBehaviours Backward();
+}
+class Soldier : IBehaviours
+{
+    public IBehaviours Run()
+    {
+        Console.WriteLine("Soldier is running...");
+        return this;
+    }
+    public IBehaviours RunRight()
+    {
+        Console.WriteLine("soldier is running right...");
+        return this;
+    }
+    public IBehaviours RunLeft()
+    {
+        Console.WriteLine("soldier is running left....");
+        return this;
+    }
+    public IBehaviours Stop()
+    {
+        Console.WriteLine("Soldier is stopping...");
+        return this;
+    }
+    public IBehaviours Backward()
+    {
+        Console.WriteLine("soldier is backward...");
+        return this;
+    }
+}
+class MainClass
+{
+    static void Main(string[] args)
+    {
+        Soldier soldier = new Soldier();
+
+        soldier.Run();
+    }
+}
